ee92cac8ec37a7bf95299990667c6d5281cb5a34
柳杨第 <sotework@outlook.com>
2022-03-02 18:03:02 +0800

    修改baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ const mode = GetViteMode();
 
 let i = 0;
 
-const basePath = '/sote-home';
+const basePath = '/sote-home/';
 const isProd = mode === 'production';
 console.log('isProd:', isProd, '\r\nmode:', mode);
 
